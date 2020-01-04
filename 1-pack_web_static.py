1fd993cfc204a26c33faf138266e6b024f8cb198
jdarangop <juandiegoap33@gmail.com>
2020-01-04 01:44:29 +0000

    Fix: pycodestyle

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -3,11 +3,15 @@
 from fabric.api import local
 import datetime
 import os
+
+
 def do_pack():
     """ Do pack """
-    if not os.path.isdir("/versions"):
+    if not os.path.isdir("./versions"):
         os.mkdir("versions")
     now = datetime.datetime.now()
-    fail = local("tar -cvzf versions/web_static_{}{}{}{}{}{}.tgz web_static".format(now.year, now.month, now.day, now.hour, now.minute, now.second), capture=False)
-    if fail.failed:
-        return None
+    fail = local("tar -cvzf versions/web_static_{}{}{}{}{}{}.tgz \
+                  web_static".format(now.year,
+                                     now.month, now.day,
+                                     now.hour, now.minute,
+                                     now.second))

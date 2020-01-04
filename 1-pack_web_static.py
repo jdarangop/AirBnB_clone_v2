99d032775bf15389ed6d9dd9842737c7e1d96117
jdarangop <juandiegoap33@gmail.com>
2020-01-04 01:22:09 +0000

    Fix: add comprobation if the directory exists

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,14 +2,12 @@
 """ Module Pack Web Static """
 from fabric.api import local
 import datetime
+import os
 def do_pack():
     """ Do pack """
+    if not os.path.isdir("/versions"):
+        os.mkdir("versions")
     now = datetime.datetime.now()
-    fail = local("tar -cvzf versions/web_static_{}{}{}{}{}{}.tgz".format(now.year,
-								  now.month,
-							          now.day,
-								  now.hour,
-								  now.minute,
-                                                                  now.second))
-     if fail.failed:
-         return None
+    fail = local("tar -cvzf versions/web_static_{}{}{}{}{}{}.tgz web_static".format(now.year, now.month, now.day, now.hour, now.minute, now.second), capture=False)
+    if fail.failed:
+        return None
